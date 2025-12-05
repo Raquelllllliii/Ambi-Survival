8a597eca1178cd93ca59c9c3b4cfaa307c3e8299
aos <jean.ambrosius@tu-dortmund.de>
2025-12-05 10:30:21 +0100

    Projekt mit Modulen strukturiert. Kommentare zur Erläuterung sind integriert.

--- src/main.lua
+++ src/main.lua
@@ -1,13 +1,56 @@
--- <- So wird eine Zeile auskommentiert (d.h. der Code/Text danach wird bei "Run" ignoriert.)
---[[ Will man über mehrere Zeilen auskommentieren, so verwendet man
-     zusätzlich eckige Klammern.
-]]--
+-- Lua ist in sogenannte Module eingeteilt - die kann man sich als eigene Dateien vorstellen, die sich um
+-- genau eine "Sache" kümmern. Ein Beispiel wäre das Modul graphics!
 
--- Dies ist eine Methode, die dauerhaft aufgerufen wird - ca. 60 mal pro Sekunde.
+-- Module müssen über main.lua geladen werden --> das passiert mit dem Schlüsselbegriff require
+-- Oft ist es so, dass ein Modul "Dinge" laden soll, von daher
+-- sollte es vor load() "required" werden.
+require "kreis"
+require "rechteck"
+require "oink"
+
+function love.load()
+    zustand = 1
+    zeit = 0
+
+    kreis.load()
+    rechteck.load()
+    oink.load()
+end
+
 function love.draw()
-    love.graphics.setBackgroundColor(100/255,0,0)              --Farbewerte sind Anteile von 255. Entspricht also rgb(100,0,0)
-    love.graphics.setColor(1,0,0)
-    love.graphics.rectangle("fill",400,100,75,25)    --Ein Rechteck, dessen linke obere Ecke bei den Koordinaten (400|100) ist. Es ist 75x25 Pixel groß.
-    love.graphics.setColor(0,1,0)
-    love.graphics.circle("line",0,0,50)                     --Ein Kreis, dessen Mittelpunkt bei den Koordinaten (0|0) ist. Er hat einen Radius von 50Pixel.
+    love.graphics.print("Zeit: " .. math.floor(zeit*100)/100, 375, 0)
+    if zustand == 1 then
+        kreis.draw()
+    end
+    if zustand == 2 then
+        rechteck.draw()
+    end
+    if zustand == 3 then
+        oink.draw()
+    end
+end
+
+function love.update(dt)
+    zeit = zeit + dt
+
+    --entweder
+    if zeit > 4 and zustand == 1 then
+        zustand = 2
+    end
+
+    if zeit > 8 and zustand == 2 then
+        zustand = 3
+    end
+
+    if zustand == 1 then
+        kreis.update(dt)
+    end
+
+    if zustand == 2 then
+        rechteck.update(dt)
+    end
+
+    if zustand == 3 then
+        oink.update(dt)
+    end
 end
